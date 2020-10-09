df8c4e2f751e69d9cee78b45e1b46486c8bf57fc
Diogo Felipe <dlcfelipp@gmail.com>
2020-10-09 08:21:56 -0300

    class that represents an pencil

---
+++ src/Pencil.js
@@ -0,0 +1,36 @@
+
+class Pencil{
+
+    constructor(color, lineWidth, context){
+        this.color = color;
+        this.lineWidth = lineWidth;
+        this.context = context;
+
+        this.atualPosition = {
+            x: 0,
+            y: 0
+        }
+        
+        this.previousPosition = {
+            x:0,
+            y:0
+        }
+    }
+
+    draw(position){
+        this.atualPosition.x = position.x;
+        this.atualPosition.y = position.y;
+    
+        this.context.beginPath();
+        this.context.moveTo(this.previousPosition.x, this.previousPosition.y);
+        this.context.lineTo(this.atualPosition.x, this.atualPosition.y);
+        this.context.strokeStyle = this.color;
+        this.context.lineWidth = this.lineWidth;
+        this.context.stroke();
+        this.context.closePath();
+    
+        this.previousPosition.x = this.atualPosition.x;
+        this.previousPosition.y = this.atualPosition.y;
+    }
+
+}

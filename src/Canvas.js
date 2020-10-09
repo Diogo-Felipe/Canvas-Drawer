c3697ab15fe36d23434c99caf562dd1b15a9e607
Diogo Felipe <dlcfelipp@gmail.com>
2020-10-09 08:21:43 -0300

    class that represents an canvas

---
+++ src/Canvas.js
@@ -0,0 +1,16 @@
+
+class Canvas {
+
+    constructor(){
+        this.tela = document.getElementById('canvas');
+        this.context = this.tela.getContext("2d");
+
+        this.pencil = new Pencil("#cc22cc", 2, this.context);
+
+        this.mouseDown = false;
+    }
+
+    clearAll(){
+        this.context.clearRect(0, 0, this.tela.width, this.tela.height);
+    }
+}
